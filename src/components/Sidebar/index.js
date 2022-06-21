98d3648854dbc4dbf8073fb56c86c400cfed66cd
yuto ichinose <yuto.0526.sano@gmail.com>
2022-06-21 20:46:42 +0900

    スライダーコンポーネント完成

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
     return (
@@ -23,7 +24,38 @@ const Sidebar = () => {
                     <FontAwesomeIcon icon={faEnvelope} color = '#4d4d4e' />
                 </NavLink>
             </nav>
-        
+            <ul>
+                <li>
+                    <a 
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://twitter.com/dimyasvariant'
+                        title='about Twitter'
+                    >
+                        <FontAwesomeIcon icon={faTwitter} color = '#4d4d4e'/>
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://github.com/illionillion'
+                        title='about GitHub'
+                    >
+                        <FontAwesomeIcon icon={faGithub} color = '#4d4d4e'/>
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://www.youtube.com/channel/UCUN7wqpoIb00mszKKzFn7HQ'
+                        title='about Youtube'
+                    >
+                        <FontAwesomeIcon icon={faYoutube} color = '#4d4d4e'/>
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
