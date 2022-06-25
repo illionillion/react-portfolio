cd9828ff4bc7a58e38eabbf9a8b2eb564573ebec
yuto ichinose <yuto.0526.sano@gmail.com>
2022-06-25 10:02:26 +0900

    Homeの部分まで、次はHome部分のアニメーション

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-s.png'
+import './index.scss'
 const Home = () => {
     return (
-        <>
-        Hello
-        </>
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi,<br/>I'm
+                <img src={LogoTitle} alt='developer'/>
+                lobodan
+                <br/>
+                web developer
+                </h1>
+                <h2>Frontend Developer / JavaScript Expert / Youtuber</h2>
+                <Link to='/contact' className ='flat-button'>CONTACK ME</Link>
+            </div>
+        </div>
     )
 }
 
